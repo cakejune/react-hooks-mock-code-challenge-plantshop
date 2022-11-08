105acffc87bfd7c688a303f6da2d99c821aadd21
cakejune <tyq444@gmail.com>
2022-11-08 10:36:59 -0500

    plant lab starting

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PlantCard from "./PlantCard";
 
 function PlantList() {
+
+const [plantData, setPlantData] = useState('');
+
+  useEffect(()=>{
+fetch("http://localhost:6001/plants")
+.then (res=>res.json())
+.then ((plants)=> {
+
+setPlantData(plants)
+
+})
+  }, [])
+  
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <div>
+      <ul className="cards">{/* render PlantCards components in here */}</ul>
+      <PlantCard />
+    </div>
   );
 }
 
